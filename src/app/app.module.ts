ee0e2f6fc88e434473defe6c5058d5d9e5b0673c
Yair Rfz <yairrdz11@gmail.com>
2021-11-26 12:53:38 -0600

    Gallery created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    CarruselComponent
+    CarruselComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule
